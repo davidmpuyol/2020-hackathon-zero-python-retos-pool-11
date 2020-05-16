9433ddd856c83e4f8c70c315e73999ba58a699e3
davidmpuyol <davidmpuyol@gmail.com>
2020-05-16 17:46:21 +0200

    kata2

--- src/kata2/rpg.py
+++ src/kata2/rpg.py
@@ -3,11 +3,17 @@
 import random
 import string
 
-def RandomPasswordGenerator(passLen=10):
-    #
-    #
-    
-    #
-    #
+posibles = string.ascii_letters + string.digits + string.punctuation
 
-    return ""
+def RandomPasswordGenerator(passLen=10):
+    cadena = ""
+    for i in range(passLen):
+        if not (chr in string.ascii_letters for chr in cadena):
+            cadena += random.choice(string.ascii_letters)
+        elif not (chr in string.digits for chr in cadena):
+            cadena += random.choice(string.digits)
+        elif not (chr in string.puctuation for chr in cadena):
+            cadena += random.choice(string.punctuation)
+        else:
+            cadena += random.choice(posibles)
+    return cadena
